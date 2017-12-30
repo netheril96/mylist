af1844cf6cad0c3ede349c9d034f46b8d0f24b72
Siyaun Ren <netheril96@gmail.com>
2017-12-30 11:01:28 +0800

    Remove duplicates and exception rules

--- fetch_and_combine.py
+++ fetch_and_combine.py
@@ -8,24 +8,22 @@ import re
 def main():
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
     GFWLIST_URL = 'https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt'
-    contents = requests.get(GFWLIST_URL).text
+    contents = requests.get(GFWLIST_URL).content
     contents = base64.b64decode(contents)
     lines = contents.split(b'\n')
     with open('myrules.txt', 'rb') as f:
-        myrule = f.read()
+        myrules = f.readlines()
+
+    lines = [l + b'\n' for l in lines if not l.startswith(b'!') and not l.startswith(b'@@')]
+    lines[1:1] = myrules
+    unique_lines = set(lines)
+
     with open('gfwlist.my.txt', 'wb') as f:
         write = f.write
-        write(lines[0])
-        write(b'\n\n')
-        write(myrule)
-        write(b'\n\n')
-
-        for i in range(1, len(lines)):
-            l = lines[i]
-            if l.startswith(b'!'):
-                continue
-            write(l)
-            write(b'\n')
+        for l in lines:
+            if l in unique_lines:
+                write(l)
+                unique_lines.discard(l)
 
 
 if __name__ == '__main__':
